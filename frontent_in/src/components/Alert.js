b980f542dc2c57e3ac6119890b8650d3c9644c0c
Ayan Manna <ayanmanna858@gmail.com>
2025-03-10 20:57:47 +0530

    63 complete

--- frontent_in/src/components/Alert.js
+++ frontent_in/src/components/Alert.js
@@ -1,14 +1,12 @@
-import React from "react";
+import React from 'react'
 
-const Alert = (props) => {
-  return (
-    <div>
-      <div class="alert alert-primary" role="alert">
-         
-         {props.message}
-      </div>
-    </div>
-  );
-};
-
-export default Alert;
+export const Alert = (props) => {
+    return (
+        <div>
+            <div className="alert alert-primary" role="alert">
+                {props.message}
+            </div>
+        </div>
+    )
+}
+export default Alert
