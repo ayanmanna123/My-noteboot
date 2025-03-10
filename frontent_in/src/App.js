4af8f87b5fea9c9e46e36176e48fedb4c6484d96
Ayan Manna <ayanmanna858@gmail.com>
2025-03-10 09:57:45 +0530

    new

--- frontent_in/src/App.js
+++ frontent_in/src/App.js
@@ -1,9 +1,29 @@
- 
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import { Home } from './components/Home';
+import About from './components/About';
+import NoteState from './context/notes/Notestate';
 
 function App() {
   return (
-    <h1>good morning</h1>
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </div>
+        </Router>
+      </NoteState>
+    </>
   );
 }
 
