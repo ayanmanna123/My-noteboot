d02f7c3bdcf29b6610b3edbe19fc4f66a62a3493
Ayan Manna <ayanmanna858@gmail.com>
2025-03-13 01:16:24 +0530

    69 complete

--- frontent_in/src/App.js
+++ frontent_in/src/App.js
@@ -9,6 +9,8 @@ import { Home } from './components/Home';
 import About from './components/About';
 import NoteState from './context/notes/Notestate';
 import Alert from './components/Alert';
+import Login from './components/Login';
+import Singup from './components/Singup';
 
 function App() {
   return (
@@ -21,6 +23,8 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/singup" element={<Singup />} />
             </Routes>
           </div>
         </Router>
